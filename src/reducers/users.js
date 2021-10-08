749037a33a1c0a5aa6cb77f2329b79857614e4f9
Steve Onyeneke <Jackes.Afonso@bp.com>
2021-10-08 14:14:40 +0100

    feat: add reducers for initial data

---
+++ src/reducers/users.js
@@ -0,0 +1,15 @@
+import { RECEIVE_USERS } from "../actions/users";
+
+// users reducer which modifies the state of the users object based on a dispatched action
+// returns a new state
+export default function users (state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_USERS:
+      return {
+        ...state,
+        ...action.users
+      }
+      default:
+        return state
+  }
+}
