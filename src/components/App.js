8f13b901dec60132f947bac3fc1e845658cef092
Steve Onyeneke <Jackes.Afonso@bp.com>
2021-10-09 10:22:44 +0100

    feat: setup dashboard component to display tweetid

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
 
 class App extends Component {
+  componentDidMount() {
+    // dispatch the invocation of the handleInitialData action creator
+    this.props.dispatch(handleInitialData())
+  }
   render() {
     return (
       <div>
-        Starter Code
+        {this.props.loading === true ? null : <Dashboard />}
       </div>
     )
   }
 }
 
-export default App
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+// add connect to have access to dispatch
+export default connect(mapStateToProps)(App)
---
+++ src/components/Dashboard.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className='center'>Your Timeline</h3>
+        <ul className='dashboard-list'>
+          {this.props.tweetIds.map((id) => (
+            <li key={id}>
+              <div>TWEET ID: {id}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+// takes in the state of our store, sorts the tweets by timestamp and return an object that has the tweets property id on it
+// uses destructuring to get only the needed tweets slice from the store
+function mapStateToProps({ tweets }) {
+  return {
+    tweetIds: Object.keys(tweets)
+    .sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
