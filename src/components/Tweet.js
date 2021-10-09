236999b8fc1ad6f58a11f54cd370fab7c674dad3
Steve Onyeneke <Jackes.Afonso@bp.com>
2021-10-09 14:08:30 +0100

    feat: build tweet ui

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import {formatTweet} from '../utils/helpers'
+import {formatTweet, formatDate} from '../utils/helpers'
+import { IoArrowUndoOutline }from 'react-icons/io5'
+import { IoHeartOutline}from 'react-icons/io5'
 
 class Tweet extends Component {
+  handleLike = (e) => {
+    e.preventDefault()
+    //todo: handle like tweet
+  }
+
+  toParent = (e, id) => {
+    e.preventDefault()
+    //todo: redirect to parent tweet
+  }
+  
   render() {
     const { tweet } = this.props
+
     if(tweet === null) {
       return <p>This Tweet doesn't exist</p>
     }
+    const {
+      name, avatar, timestamp, text, hasLiked, likes, replies, parent
+    } = tweet
+
     return (
       <div className='tweet'>
+        <img src={avatar} alt={`Avatar of {name}`} className='avatar'/>
+        <div className='tweet-info'>
+          <div>
+            <span>{name}</span>
+            <div>{formatDate(timestamp)}</div>
+            {parent && (
+              <button className='replying-to' onClick={(e) => this.toParent(e, parent.id)}>
+                Replying to @{parent.author}
+              </button>
+            )}
+            <p>{text}</p>
+          </div>
+          <div className='tweet-icons'>
+            <IoArrowUndoOutline className='tweet-icon'/>
+            <span>{replies !== 0 && replies}</span>
+            <button className='heart-button' onClick={this.handleLike}>
+              {hasLiked === true 
+              ? <span>liked!<IoHeartOutline color='#e0245e' className='tweet-icon' /></span>
+              : <IoHeartOutline className='tweet-icon' />}
+            </button>
+            <span>{likes !== 0 && likes}</span>
+          </div>
+        </div>
 
-
+        
       </div>
     )
   }
